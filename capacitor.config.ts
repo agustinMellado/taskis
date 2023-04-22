448af328a9b60b14846e7cefcfe848af2aa1bf22
Agustin Mellado <aguxmellado@gmail.com>
2023-04-22 16:11:19 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app_tareas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
