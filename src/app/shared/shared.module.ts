e5857a88eaa0da7da256383f1849aef4795d2ba8
Agustin Mellado <aguxmellado@gmail.com>
2023-04-22 16:32:51 -0300

    crecion de modulo compartido, se agregaron importaciones para  formularios, rutas y uso de frame

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
