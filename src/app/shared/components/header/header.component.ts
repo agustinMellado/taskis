66ad49c72851ff68cdc2b7df2b2ce0db18cdd84e
Agustin Mellado <aguxmellado@gmail.com>
2023-04-22 17:26:57 -0300

    se crearon variables hijas para compartirse a un componente padre

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit {
+  //Uso el decorador Input, pasando informacion del hijo al padre
+  @Input() titulo: string;
+  @Input() botonAtras: string; //para agregar ruta p/retroceder
+  @Input() isModal: string; //para verificar si estamos usando el header en una modal
+  @Input() color: string;
+  @Input() centerTitle: boolean;
+  
+  constructor() {}
 
   ngOnInit() {}
-
 }
