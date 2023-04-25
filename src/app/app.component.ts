b923e5c6833bd879b0b5b81d3664bb60e322212a
Agustin Mellado <aguxmellado@gmail.com>
2023-04-25 16:19:29 -0300

    se creo una funcion para tomar el cambio en el localStorage al momento de cambiar de modo dark/light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private themeSvc: ThemeService) {
+    //llama a la funcion para que cada vez que se actualice la pagina los cambios se mantengan.
+    this.themeSvc.setInitialTheme()
+  }
 }
--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -8,6 +8,18 @@ export class ThemeService {
   //Variable que escucha los cambios y los refleja en todos los componentes.
   darkMode = new BehaviorSubject(false);
   constructor() { }
+
+  setInitialTheme() {
+    //una vez obtenido el valor necesitamos volverlo booleano
+    let darkMode = JSON.parse(localStorage.getItem('darkMode'));
+    if (darkMode){
+      document.body.setAttribute('color-theme','dark')
+    }else{
+      document.body.setAttribute('color-theme','light')
+    }
+
+  }
+
 //funcion para pone en modo oscuro la aplicacion
   setTheme(darkMode: boolean){
     if(darkMode){
@@ -18,5 +30,7 @@ export class ThemeService {
     }
     //escucha los cambios y los aplica.
     this.darkMode.next(darkMode);
+    //guarda el cambio aplicado
+    localStorage.setItem('darkMode',JSON.stringify(darkMode))
   }
 }
