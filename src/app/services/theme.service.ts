6f4c4bac1f7ec456e87bf9d87fb928880e8d0a4e
Agustin Mellado <aguxmellado@gmail.com>
2023-04-25 22:01:20 -0300

    correccion y aplicacion de funcion creada

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -2,35 +2,34 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
   //Variable que escucha los cambios y los refleja en todos los componentes.
   darkMode = new BehaviorSubject(false);
-  constructor() { }
+  constructor() {}
 
   setInitialTheme() {
     //una vez obtenido el valor necesitamos volverlo booleano
     let darkMode = JSON.parse(localStorage.getItem('darkMode'));
-    if (darkMode){
-      document.body.setAttribute('color-theme','dark')
-    }else{
-      document.body.setAttribute('color-theme','light')
+    if (darkMode) {
+      this.setTheme(darkMode);
+    } else {
+      this.setTheme(darkMode);
     }
-
   }
 
-//funcion para pone en modo oscuro la aplicacion
-  setTheme(darkMode: boolean){
-    if(darkMode){
+  //funcion para pone en modo oscuro la aplicacion
+  setTheme(darkMode: boolean) {
+    if (darkMode) {
       //cambiamos el modo a dark
-      document.body.setAttribute('color-theme','dark')
-    }else{
-      document.body.setAttribute('color-theme','light')
+      document.body.setAttribute('color-theme', 'dark');
+    } else {
+      document.body.setAttribute('color-theme', 'light');
     }
     //escucha los cambios y los aplica.
     this.darkMode.next(darkMode);
     //guarda el cambio aplicado
-    localStorage.setItem('darkMode',JSON.stringify(darkMode))
+    localStorage.setItem('darkMode', JSON.stringify(darkMode));
   }
 }
