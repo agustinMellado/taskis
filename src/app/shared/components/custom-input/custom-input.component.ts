cd7e7409101bbf8189923ab655ed165721396294
Agustin Mellado <aguxmellado@gmail.com>
2023-04-27 15:54:17 -0300

    creacion de variables

--- src/app/shared/components/custom-input/custom-input.component.ts
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-input',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomInputComponent  implements OnInit {
 
+  @Input() control: FormControl;
+  @Input() label: string;
+  @Input() icon: string;
+  @Input() type: string;
+  @Input() autocomplete: string;
+
+  
+
+  
   constructor() { }
 
   ngOnInit() {}
