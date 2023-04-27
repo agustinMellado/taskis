2627598f22443564ba7e17db136fbf759cdb670c
Agustin Mellado <aguxmellado@gmail.com>
2023-04-26 22:54:51 -0300

    creacion de login con sus variables de control

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-
+  form = new FormGroup({
+    email: new FormControl('',[Validators.required, Validators.email ]),
+    password: new FormControl('',[Validators.required])
+  })
   constructor() { }
 
   ngOnInit() {
