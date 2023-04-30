87df49715b04a566823d24f494db8661290d7ea0
Agustin Mellado <aguxmellado@gmail.com>
2023-04-30 16:48:19 -0300

    configuracion para usar firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDrcjadkRTyD35qK_ukCu8CZrZEt9nqj8g',
+    authDomain: 'taskis-f4075.firebaseapp.com',
+    projectId: 'taskis-f4075',
+    storageBucket: 'taskis-f4075.appspot.com',
+    messagingSenderId: '204098057976',
+    appId: '1:204098057976:web:e5de3fe27920cef8cfed0d',
+  },
 };
