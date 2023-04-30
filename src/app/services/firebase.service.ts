ed0670b2cc8789282a61ac5c3bb11f8b762285c5
Agustin Mellado <aguxmellado@gmail.com>
2023-04-30 18:24:35 -0300

    funciones de logeo y registro

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseService {
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+  //autentificacion
 
-  constructor(
-    private auth:AngularFireAuth,
-    private db: AngularFirestore
-  ) { }
+  //
+  login(user: User) {
+    return this.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
+  //Registro
+  signUp(user: User) {
+    return this.auth.signInWithEmailAndPassword(user.email, user.password);
+  }
 }
