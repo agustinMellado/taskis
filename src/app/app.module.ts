8165687cf90ade23e222c63181e4b004dec9b470
Agustin Mellado <aguxmellado@gmail.com>
2023-04-30 17:22:16 -0300

    importaciones necesarias para poder usar la base de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+//importacion de firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';//funciona para authenticar al usuario
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';//para poder usar la base de datos.
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
