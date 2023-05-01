cac20e9d1de1b43abe9a841e6c37fd814a981e2f
Agustin Mellado <aguxmellado@gmail.com>
2023-05-01 18:08:17 -0300

    inyeccion de servicios y creacion de funcion para cerrar sesion

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -3,12 +3,17 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { User } from '../models/user.model';
 import { getAuth, updateProfile } from 'firebase/auth';
+import { UtilsService } from './utils.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private utilsSvc: UtilsService
+  ) {}
   //autentificacion
 
   //logueo
@@ -23,11 +28,18 @@ export class FirebaseService {
   //Actualizar usuario
   updateUser(user: any) {
     const auth = getAuth();
-    return updateProfile(auth.currentUser, user)
+    return updateProfile(auth.currentUser, user);
   }
-//retornamos el estado de la autenticacion del usuario.
+  //retornamos el estado de la autenticacion del usuario.
   getAuthState() {
-    return this.auth.authState
-
+    return this.auth.authState;
+  }
+  //Cerrar sesion
+  async signOut() {
+    await this.auth.signOut();
+    //redirecciono la url
+    this.utilsSvc.routerLink('/auth');
+    //funcion para remover el user del localStorage
+    localStorage.removeItem('user');
   }
 }
