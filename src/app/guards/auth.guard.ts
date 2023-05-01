8d239d73f99f6a73266d787b937cf36705486c50
Agustin Mellado <aguxmellado@gmail.com>
2023-05-01 17:39:44 -0300

    estructura condicional para los guards

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -5,7 +5,7 @@ import {
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { UtilsService } from '../services/utils.service';
 import { FirebaseService } from '../services/firebase.service';
 
@@ -25,6 +25,19 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return true;
+    //aplicacion de guard para prevenir que entren sin un usuario creado.
+    return this.firebaseSvc.getAuthState().pipe(
+      map((auth) => {
+        //si existe usuario autenticado
+        if (auth) {
+          return true;
+          //No existe usuario autenticado
+        } else {
+          //si es falso aplico el guard y redirijo la ruta.
+          this.utilsSvs.routerLink('/auth');
+          return false;
+        }
+      })
+    );
   }
 }
--- src/app/guards/no-auth.guard.ts
+++ src/app/guards/no-auth.guard.ts
@@ -1,15 +1,43 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { UtilsService } from '../services/utils.service';
+import { FirebaseService } from '../services/firebase.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoAuthGuard implements CanActivate {
+  constructor(
+    private firebaseSvc: FirebaseService,
+    private utilsSvs: UtilsService
+  ) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    //aplicacion de guard para prevenir que entren sin un usuario creado.
+    return this.firebaseSvc.getAuthState().pipe(
+      map((auth) => {
+        //Si no existe usuario autenticado.
+        if (!auth) {
+          return true;
+        //si existe usuario autenticado.
+        } else {
+          //llamo a la funcion y redirijo.
+          this.utilsSvs.routerLink('/tabs/home');
+          return false;
+        }
+      })
+    );
   }
-  
 }
