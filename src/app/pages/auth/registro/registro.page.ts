b5ad779f0193c46bf9bbe20afa594154389f7689
Agustin Mellado <aguxmellado@gmail.com>
2023-05-01 10:24:00 -0300

    inyeccion de toast y creacion de funcion para mostrar mensaje, configuracion de estructura de registro llamando a las funciones del servicio utils

--- src/app/pages/auth/registro/registro.page.ts
+++ src/app/pages/auth/registro/registro.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 import { CustomValidators } from 'src/app/utils/custom-validators';
 @Component({
   selector: 'app-registro',
@@ -12,10 +13,16 @@ export class RegistroPage implements OnInit {
   form = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(9)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(9),
+    ]),
     confirmPassword: new FormControl(''),
   });
-  constructor(private firebaseSvc: FirebaseService) {}
+  constructor(
+    private firebaseSvc: FirebaseService,
+    private utilsSvc: UtilsService
+  ) {}
 
   ngOnInit() {
     this.confirmPasswordValidator();
@@ -33,20 +40,49 @@ export class RegistroPage implements OnInit {
   //toma lo que se escribio en el formulario.
   submit() {
     if (this.form.valid) {
-      console.log(this.form.value);
-    }//primero registramos el usuario
-    this.firebaseSvc.signUp(this.form.value as User).then(async res=>{
-      console.log(res)
-      //luego actualizamos el nombre el usuario tomando el valor del input del registro
-      await this.firebaseSvc.updateUser({displayName: this.form.value.name}) //displayName permite editar la variable del usuario
-      //variable local para guardar el usuario
-      let user: User ={//estructura
-        uid: res.user.uid,
-        name: res.user.displayName,
-        email: res.user.email
-      }
-    }, error=>{
-      console.log(error);
-    })
+      
+      //mostrar carga de datos
+      this.utilsSvc.presentLoading({message:'Registrando...'})
+      //primero registramos el usuario
+      this.firebaseSvc.signUp(this.form.value as User).then(
+        async (res) => {
+          console.log(res);
+          //luego actualizamos el nombre el usuario tomando el valor del input del registro
+          await this.firebaseSvc.updateUser({
+            displayName: this.form.value.name,
+          }); //displayName permite editar la variable del usuario
+          //variable local para guardar el usuario
+          let user: User = {
+            //estructura
+            uid: res.user.uid,
+            name: res.user.displayName,
+            email: res.user.email,
+          };
+          //mandamos los datos para guardar
+          this.utilsSvc.setElementInLocalStorage('user', user);
+          //enrutamiento de tabs
+          this.utilsSvc.routerLink('/tabs');
+          //una vez que se carguen los datos, finalizamos la carga.
+          this.utilsSvc.dismissLoading();
+          //mesaje por pantalla
+          this.utilsSvc.presentToast({
+            message:`Te damos la Bienvenida ${user.name}`,
+            duration: 1500,
+            color: 'primary',
+            icon: 'preson-outline'
+          })
+        },
+        (error) => {
+          this.utilsSvc.presentToast({
+            message:error,
+            duration: 5500,
+            color: 'warning',
+            icon: 'alert-circle-outline'
+          })
+          //una vez que se carguen los datos, finalizamos la carga.
+          this.utilsSvc.dismissLoading()
+        }
+      );
+    }
   }
 }
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoadingController, LoadingOptions } from '@ionic/angular';
+import {
+  LoadingController,
+  LoadingOptions,
+  ToastController,
+  ToastOptions,
+} from '@ionic/angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UtilsService {
-
   constructor(
     private loadingController: LoadingController,
     private router: Router,
-  ) { }
-//LOADING
-//forma de carga asincrona de los datos
-  async presentLoading(opts:LoadingOptions) {
+    private toastController: ToastController
+  ) {}
+  //LOADING
+  //muestra que se estan cargando los datos
+  async presentLoading(opts?: LoadingOptions) {
     const loading = await this.loadingController.create(opts);
     await loading.present();
   }
@@ -21,22 +26,24 @@ export class UtilsService {
   //funcion para ocultar la carga de datos
   async dismissLoading() {
     return await this.loadingController.dismiss();
-
   }
   //LOCALSTORAGE
   //guardado de datos en firestore
-  setElementInLocalStorage(key:string, element:any){
+  setElementInLocalStorage(key: string, element: any) {
     //conversion a string necesaria para guardar los datos
     return localStorage.setItem(key, JSON.stringify(element));
-
   }
   //Tomar datos del localStorage
-  getElementFrontLocalStorage(key: string){
-    return JSON.parse(localStorage.getItem(key))
+  getElementFrontLocalStorage(key: string) {
+    return JSON.parse(localStorage.getItem(key));
   }
-
+  //para mostrar un mensaje por pantalla
+  async presentToast(opts: ToastOptions) {
+    const toast = await this.toastController.create(opts);
+    toast.present(); 
+  }
   //routeo para trabajar con la url
-  routerLink(url:string){
+  routerLink(url: string) {
     return this.router.navigateByUrl(url);
   }
 }
