95f1a33dcdb624b3483dd467a7af910872906572
Agustin Mellado <aguxmellado@gmail.com>
2023-05-01 18:50:20 -0300

    creacion de funciones para tomar los datos de cada usuario que entre a la pagina

--- src/app/pages/tabs/profile/profile.page.ts
+++ src/app/pages/tabs/profile/profile.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,17 +9,27 @@ import { FirebaseService } from 'src/app/services/firebase.service';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
+  //creacion de variable apra objeto que almacena  la informacion del usuario.
+  user = {} as User;
   constructor(
-    private firebaseSvc: FirebaseService
-  ) { }
+    private firebaseSvc: FirebaseService,
+    private utilsSvc: UtilsService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  //funcion para cargar informacion de cada usuario que entra a la pagina.
+  ionViewWillEnter() {
+    //se ejecuta cuando el usuario entra a la pagina
+    this.getUser()
   }
+
   //cerrar sesion
   signOut() {
     //llamo a la funcion cerrar sesion
     this.firebaseSvc.signOut();
   }
-
+  //tomo informacion del usuario
+  getUser() {
+    return (this.user = this.utilsSvc.getElementFrontLocalStorage('user'));
+  }
 }
