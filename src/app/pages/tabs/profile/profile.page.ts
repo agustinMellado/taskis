53ce54571e4c0a516e7573dd6374de91202a3e39
Agustin Mellado <aguxmellado@gmail.com>
2023-05-01 18:20:24 -0300

    inyeccion de servicio, creacion de boton y funcion para cerrar sesion

--- src/app/pages/tabs/profile/profile.page.ts
+++ src/app/pages/tabs/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private firebaseSvc: FirebaseService
+  ) { }
 
   ngOnInit() {
+  }
+  //cerrar sesion
+  signOut() {
+    //llamo a la funcion cerrar sesion
+    this.firebaseSvc.signOut();
   }
 
 }
