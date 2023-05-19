0cc828f9ac4de7c3b1b0f22013161c16c4464898
Agustin Mellado <aguxmellado@gmail.com>
2023-05-18 22:51:29 -0300

    creacion de funciones para borrar una tarea con confirmacion y actualizacion de pagina

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -12,7 +12,7 @@ import { AgregarActualizarTareaComponent } from 'src/app/shared/components/agreg
 export class HomePage implements OnInit {
   //Variable que va a almacenar una cadena de objetos
   tareas: Tareas[] = [];
-
+  user = {} as User;
   constructor(
     private firebaseSvc: FirebaseService,
     private utilsSvc: UtilsService
@@ -21,8 +21,12 @@ export class HomePage implements OnInit {
   ngOnInit() {}
   //Al entrar a la pagina inicia y muestra la funcion
   ionViewWillEnter() {
-    //llama a esta funcion
+    //llama a estas funciones
     this.getTask();
+    this.getUser();
+  }
+  getUser() {
+    return (this.user = this.utilsSvc.getElementFromLocalStorage('user'));
   }
 
   getPorcentaje(tarea: Tareas) {
@@ -31,14 +35,14 @@ export class HomePage implements OnInit {
   }
   async agregarOActualizarTarea(tarea?: Tareas) {
     //funcion para traer las tareas una vez que esta se haya creado/actualizado
-    let res= await this.utilsSvc.presentModal({
+    let res = await this.utilsSvc.presentModal({
       component: AgregarActualizarTareaComponent,
       componentProps: { tarea },
       cssClass: 'agregar-actualizar-modal',
     });
     //si existe, llamamos para que actualice la vista
-    if (res&&res.success){
-    this.getTask()
+    if (res && res.success) {
+      this.getTask();
     }
   }
   // agarro las tareas
@@ -53,6 +57,59 @@ export class HomePage implements OnInit {
         this.tareas = res;
         sub.unsubscribe(); //aplicamos unsubscribe para no saturar a la base de datos con consultas.
       },
+    });
+  }
+  //funcion para borrar
+  borrarTarea(tarea: Tareas) {
+    let path = `users/${this.user.uid}/tareas/${tarea.id}`; //le paso la ruta exacta de donde se encuentra
+    //como es una petision a la bd y puede tardar llamo a la funcion
+    this.utilsSvc.presentLoading();
+
+    //realizo la promesa para borrar el objeto a la coleccion.
+    this.firebaseSvc.deleteDocument(path).then(
+      (res) => {
+        //muestro mensaje
+        this.utilsSvc.presentToast({
+          message: 'Tarea eliminada exitosamente!',
+          color: 'success',
+          icon: 'checkmark-circle-outline',
+          duration: 1500,
+        });
+        //cuando termine de eliminar la tarea actualizamos vista
+        this.getTask()
+        this.utilsSvc.dismissLoading();
+      },
+      (error) => {
+        //muestro mensaje
+        this.utilsSvc.presentToast({
+          message: error,
+          color: 'warning',
+          icon: 'alert-checkmark-circle-outline',
+          duration: 5500,
+        });
+        this.utilsSvc.dismissLoading();
+      }
+    );
+  }
+  //funcion de confirmacion
+  confirmarDelete(tarea:Tareas){
+    this.utilsSvc.presentAlert({
+      header: 'eliminar tarea',
+      message: '¿Quieres eliminar esta tarea?',
+      mode: 'md',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Si, eliminar',
+          handler: () => {
+            //eliminamos la tarea
+            this.borrarTarea(tarea)
+          },
+        },
+      ],
     });
   }
 }
